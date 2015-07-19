4a693a38fe1848129064dd98577b3afe03b3fe01
Felix Östh <felixosth@gmail.com>
2015-07-19 11:30:19 +0200

    Added project

---
+++ UpgradingSystem/Assets/UpgradeSystem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpgradeSystem : MonoBehaviour {
+
+    public Text text;
+    public string CurrencyName = "Fefferinos";
+
+    public float Gold
+    {
+        get
+        {
+            return _gold;
+        }
+        set
+        {
+            if(text != null) // gold display
+                text.text = value + " " + CurrencyName;
+            _gold = value;
+        }
+    }
+    private float _gold;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Gold = 5000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void PressedUpgrade(UpgradeButton button)
+    {
+    }
+
+
+    void Upgrade(float value)
+    {
+        value += 1;
+    }
+
+
+    void Upgrade(bool value)
+    {
+        value = !value;
+    }
+}
---
+++ UpgradingSystem/Assets/Upgradeable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upgradeable : MonoBehaviour {
+
+
+    // sry but store all values in this array
+    // index 0 could be hp, index 1 could be attack, idk
+    public float[] FloatValues;
+
+    public bool[] BoolValues;
+}
